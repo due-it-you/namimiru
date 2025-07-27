b0dd0804278f44f033bd759ea7af6ed2749dfbd5
dueityou <dueityou@gmail.com>
2025-07-27 22:27:02 +0900

    [add] LIFFを初期化する処理の追加 #117

--- app/javascript/controllers/liff_controller.js
+++ app/javascript/controllers/liff_controller.js
@@ -3,5 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="liff"
 export default class extends Controller {
   connect() {
+    liff.init({
+      liffId: ENV["LIFF_ID"],
+    })
+    .then(() => {
+        console.log("LIFF initialized");
+      })
+    .catch(() => {
+        console.error("LIFF init failed", err);
+      })
   }
 }
