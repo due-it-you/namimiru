c3e8fcb73ef22d6fc32b9605c6c89279c691ff2e
dueityou <dueityou@gmail.com>
2025-07-29 15:30:03 +0900

    [add] 記録作成画面のページを開いた時にスライダーの初期値が画面に表示 #121

--- app/javascript/controllers/logs_slider_controller.js
+++ app/javascript/controllers/logs_slider_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="logs-slider"
 export default class extends Controller {
+  static targets = ["score", "output"]
+
   connect() {
+    this.outputTarget.textContent = this.scoreTarget.value;
   }
 }
