4b02efa0fe1858d64c4960136a9ba20a7796625e
dueityou <dueityou@gmail.com>
2025-07-31 08:35:10 +0900

    [add] usersテーブルの新規作成 #123

---
+++ db/migrate/20250730222848_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_enum :user_role, ["unclear", "person_with_bipolar", "supporter"]
+
+    create_table :users do |t|
+      t.string :line_user_id, null: false, index: { unique: true }
+      t.enum :role, enum_type: "user_role", default: "unclear", null: false
+      t.timestamps
+    end
+  end
+end
