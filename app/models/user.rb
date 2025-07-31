4d1d8e4909425143fb0dd9c5d6c99d812421ebb1
dueityou <dueityou@gmail.com>
2025-07-31 11:13:11 +0900

    [fix] lint error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   enum role: {
          unclear: "unclear",
          person_with_bipolar: "person_with_bipolar",
-         supporter: "supporter",
+         supporter: "supporter"
        }
 end
--- db/migrate/20250730222848_create_users.rb
+++ db/migrate/20250730222848_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
-    create_enum :user_role, ["unclear", "person_with_bipolar", "supporter"]
+    create_enum :user_role, [ "unclear", "person_with_bipolar", "supporter" ]
 
     create_table :users do |t|
       t.string :line_user_id, null: false, index: { unique: true }
