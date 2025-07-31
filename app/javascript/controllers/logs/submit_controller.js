44753a5fdc751213f004283e375d3a74f273972b
dueityou <dueityou@gmail.com>
2025-07-31 16:26:22 +0900

    [add] logs/new.html.erbにStimulusのlogs/submitコントローラーを適用 #124

--- app/javascript/controllers/logs/submit_controller.js
+++ app/javascript/controllers/logs/submit_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="logs--submit"
 export default class extends Controller {
+  target = ["mood_score"]
+
   connect() {
   }
 }
