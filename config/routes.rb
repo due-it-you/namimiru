de9e6d0ea93f26bc0d468bcc86cf40013bd9bf94
dueityou <dueityou@gmail.com>
2025-08-03 16:06:00 +0900

    [add] uid_sessions#createのルーティングの追加 #124

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "static_pages/privacy_policy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :logs, only: %i[new]
+  resource :uid_sessions, only: %i[create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
