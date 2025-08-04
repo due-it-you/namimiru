be166844caf2baac51a190b4d248b1920a0a2d3d
dueityou <dueityou@gmail.com>
2025-08-04 16:15:19 +0900

    [add] IDトークンの検証とプロフィール情報の取得 #124

--- app/controllers/uid_sessions_controller.rb
+++ app/controllers/uid_sessions_controller.rb
@@ -1,5 +1,16 @@
+require "net/http"
+require "uri"
+
 class UidSessionsController < ApplicationController
   def create
+    id_token = params[:id_token]
+
+    uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
+    req = Net::HTTP::Post.new(uri)
+    req["Content-Type"] = "application/x-www-form-urlencoded"
+    req.set_form_data({ "id_token" => id_token, "client_id" => ENV["LINE_LIFF_CHANNEL_ID"] })
+    res = Net::HTTP.new(uri.hostname, uri.port).start { |http| http.request(req) }
+    res_json = JSON.parse(res.body)
   end
 
   private
