9909e01db29359db2c08a5776f77508b9837fef5
dueityou <dueityou@gmail.com>
2025-08-07 00:31:18 +0900

    [add] IDトークンの検証とUIDの取得処理の実装 #127

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    # IDトークンの検証 / UIDの取得
+    id_token = params[:id_token]
+
+    uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
+    req = Net::HTTP::Post.new(uri)
+    req["Content-Type"] = "application/x-www-form-urlencoded"
+    req.set_form_data({ "id_token" => id_token, "client_id" => ENV["LINE_LIFF_CHANNEL_ID"] })
+    http = Net::HTTP.new(uri.hostname, uri.port)
+    http.use_ssl = true
+    res = http.start { |http| http.request(req) }
+    result = JSON.parse(res.body)
+    uid = result["sub"]
   end
 
   private
