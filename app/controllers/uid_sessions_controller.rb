9b696ba63863fca61028a2c812ae4e3a792a7a4d
dueityou <dueityou@gmail.com>
2025-08-08 19:10:39 +0900

    [refactor] IDトークン検証&UID取得処理のコードが冗長的であったので一行で行えるようにリファクタリングしました　#127

--- app/controllers/uid_sessions_controller.rb
+++ app/controllers/uid_sessions_controller.rb
@@ -4,14 +4,8 @@ require "uri"
 class UidSessionsController < ApplicationController
   def create
     id_token = idtoken_params[:id_token]
-
-    uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
-    req = Net::HTTP::Post.new(uri)
-    req["Content-Type"] = "application/x-www-form-urlencoded"
-    req.set_form_data({ "id_token" => id_token, "client_id" => ENV["LINE_LIFF_CHANNEL_ID"] })
-    http = Net::HTTP.new(uri.hostname, uri.port)
-    http.use_ssl = true
-    res = http.start { |http| http.request(req) }
+    channel_id = ENV["LINE_LIFF_CHANNEL_ID"]
+    res = Net::HTTP.post_form(URI.parse("https://api.line.me/oauth2/v2.1/verify"), { "id_token" => id_token, "client_id" => channel_id })
     result = JSON.parse(res.body)
     uid = result["sub"]
 
