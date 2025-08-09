8e5cb799d9674eedb8b901dc255de7953a45eb9b
dueityou <dueityou@gmail.com>
2025-08-09 21:07:02 +0900

    [fix] lint error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   enum role: {
          unclear: "unclear",
          person_with_bipolar: "person_with_bipolar",
-         supporter: "supporter",
+         supporter: "supporter"
        }
 end
