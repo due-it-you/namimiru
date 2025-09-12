22209dfc2edabce1d769ae072f28b13784017e8e
dueityou <dueityou@gmail.com>
2025-09-12 16:45:46 +0900

    feat: #9 パスワードのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable
 
   validates :email, presence: true, format: { with: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/}
+  validates :password, presence: true, confirmation: true, length: { in: 8..72 }, 
+    format: {
+      # 少なくとも1つ 英字小文字, 大文字, 数字 が含まれる / 合計で8文字以上
+      with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[!-~]{8,}+\z/,
+      message: 'は8文字以上で、大文字・小文字・数字を含めて入力してください。'
+    }
 end
