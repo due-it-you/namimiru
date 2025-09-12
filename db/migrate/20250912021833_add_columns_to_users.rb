2a87c349200754c492a3cb0e1216d2f070c9f522
dueityou <dueityou@gmail.com>
2025-09-12 11:32:58 +0900

    chore: #8 usersテーブルにカラムを追加するマイグレーションファイルの作成

---
+++ db/migrate/20250912021833_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    change_table :users do |t|
+      t.string :name, null: false
+      t.boolean :is_guest, null: false, default: false
+  end
+end
