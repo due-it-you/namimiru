39ee918ade6d45af623842b4748b01de45a656df
dueityou <dueityou@gmail.com>
2025-09-12 11:34:19 +0900

    fix: #8 endの付け忘れによるエラー

--- db/migrate/20250912021833_add_columns_to_users.rb
+++ db/migrate/20250912021833_add_columns_to_users.rb
@@ -3,5 +3,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.2]
     change_table :users do |t|
       t.string :name, null: false
       t.boolean :is_guest, null: false, default: false
+    end
   end
 end
