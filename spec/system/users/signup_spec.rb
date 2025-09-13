772829f361daa75ea0dc43233b2c7eaf7ab89f52
dueityou <dueityou@gmail.com>
2025-09-13 15:14:59 +0900

    test: #76 ユーザーネームが13文字以上の場合のテストの実装

--- spec/system/users/signup_spec.rb
+++ spec/system/users/signup_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe "Signup", type: :system do
     # 異常系
     context 'フォームの入力値が異常' do
       before do
+        visit new_user_registration_path
         fill_in 'name', with: '松永 拓朗'
         fill_in 'email', with: 'polarbear1001@gmail.com'
         fill_in 'password', with: 'G473888g!'
@@ -27,13 +28,21 @@ RSpec.describe "Signup", type: :system do
         click_on '送信'
       end
 
-      context 'ユーザーネームのフォームに何も入力されていない場合' do
+      context 'ユーザーネームのフィールドに何も入力されていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          visit new_user_registration_path
           fill_in 'name', with: ''
           click_on '送信'
           expect(page).to have_content('ユーザーネームを入力してください。')
           expect(page).to have_current_path(new_user_registration_path)
+        end
+      end
+
+      context 'ユーザーネームが13文字以上の場合' do
+        it '新規登録に失敗し、エラーメッセージが表示される' do
+          fill_in 'name', with: 'ひらかたチャンネルの３号！'
+          click_on '送信'
+          expect(page).to have_current_path(new_user_registration_path)
+          expect(page).to have_content('ユーザーネームは12文字以内にしてください。')
         end
       end
     end
