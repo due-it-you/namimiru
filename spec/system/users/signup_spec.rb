274b606f01f69c875d4fbbf8749bcc0972466a2e
dueityou <dueityou@gmail.com>
2025-09-14 01:14:28 +0900

    test: #76 変更したラベル名に合わせるようにテスト側も変更

--- spec/system/users/signup_spec.rb
+++ spec/system/users/signup_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe "Signup", type: :system do
     context 'フォームの入力値が正常' do
       it 'ユーザーの新規作成が成功する' do
         visit new_user_registration_path
-        fill_in 'name', with: '松永 拓朗'
-        fill_in 'email', with: 'polarbear1001@gmail.com'
-        fill_in 'password', with: 'G473888g!'
-        fill_in 'password_confirmation', with: 'G473888g!'
+        fill_in 'ユーザーネーム', with: '松永 拓朗'
+        fill_in 'メールアドレス', with: 'polarbear1001@gmail.com'
+        fill_in 'パスワード', with: 'G473888g!'
+        fill_in 'パスワード（確認用）', with: 'G473888g!'
         click_on '送信'
         expect(page).to have_content('新規登録が完了しました。')
         expect(page).to have_current_path('/logs/new')
@@ -21,16 +21,16 @@ RSpec.describe "Signup", type: :system do
     context 'フォームの入力値が異常' do
       before do
         visit new_user_registration_path
-        fill_in 'name', with: '松永 拓朗'
-        fill_in 'email', with: 'polarbear1001@gmail.com'
-        fill_in 'password', with: 'G473888g!'
-        fill_in 'password_confirmation', with: 'G473888g!'
+        fill_in 'ユーザーネーム', with: '松永 拓朗'
+        fill_in 'メールアドレス', with: 'polarbear1001@gmail.com'
+        fill_in 'パスワード', with: 'G473888g!'
+        fill_in 'パスワード（確認用）', with: 'G473888g!'
       end
 
       # ユーザーネーム
       context 'ユーザーネームのフィールドに何も入力されていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'name', with: ''
+          fill_in 'ユーザーネーム', with: ''
           click_on '送信'
           expect(page).to have_content('ユーザーネームを入力してください。')
           expect(page).to have_current_path(new_user_registration_path)
@@ -39,7 +39,7 @@ RSpec.describe "Signup", type: :system do
 
       context 'ユーザーネームが13文字以上の場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'name', with: 'ひらかたチャンネルの３号！'
+          fill_in 'ユーザーネーム', with: 'ひらかたチャンネルの３号！'
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('ユーザーネームは12文字以内にしてください。')
@@ -49,7 +49,7 @@ RSpec.describe "Signup", type: :system do
       # メールアドレス
       context 'メールアドレスのフィールドに何も入力されていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'email', with: ''
+          fill_in 'メールアドレス', with: ''
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('メールアドレスを入力してください。')
@@ -58,7 +58,7 @@ RSpec.describe "Signup", type: :system do
 
       context 'メールアドレスに「@」が入力されていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'email', with: 'polarbeat1001gmail.com'
+          fill_in 'メールアドレス', with: 'polarbeat1001gmail.com'
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('メールアドレスの形式を満たしてください。')
@@ -68,8 +68,8 @@ RSpec.describe "Signup", type: :system do
       # パスワード
       context 'パスワードが入力されていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'password', with: ''
-          fill_in 'password_confirmation', with: ''
+          fill_in 'パスワード', with: ''
+          fill_in 'パスワード（確認用）', with: ''
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('パスワードを入力してください。')
@@ -78,8 +78,8 @@ RSpec.describe "Signup", type: :system do
 
       context 'パスワードが8文字未満の場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'password', with: 'Hg6gggg'
-          fill_in 'password_confirmation', with: 'Hg6gggg'
+          fill_in 'パスワード', with: 'Hg6gggg'
+          fill_in 'パスワード（確認用）', with: 'Hg6gggg'
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('パスワードは8文字以上にしてください。')
@@ -88,8 +88,8 @@ RSpec.describe "Signup", type: :system do
 
       context '英字小文字・英字大文字・数字がそれぞれ1つ以上含まれていない場合' do
         it '新規登録に失敗し、エラーメッセージが表示される' do
-          fill_in 'password', with: 'h89977442xg'
-          fill_in 'password_confirmation', with: 'h89977442xg'
+          fill_in 'パスワード', with: 'h89977442xg'
+          fill_in 'パスワード（確認用）', with: 'h89977442xg'
           click_on '送信'
           expect(page).to have_current_path(new_user_registration_path)
           expect(page).to have_content('パスワードは、英字小文字・英字大文字・数字がそれぞれ1つ以上含まれるようにしてください。')
