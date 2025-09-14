838148c366e748bb0e902ea92c754bfc3f84ea0a
dueityou <dueityou@gmail.com>
2025-09-14 16:57:56 +0900

    feat: #76 バリデーションのエラーメッセージを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable
 
-  validates :name, presence: true, length: { maximum: 12, message: "は12文字以内にしてください。" }
-  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/ }
-  validates :password, presence: true, confirmation: true, length: { in: 8..72 },
+  validates :name, presence: { message: 'を入力してください。' }, length: { maximum: 12, message: "は12文字以内にしてください。" }
+  validates :email, presence: { message: 'を入力してください。' }, format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/, message: "の形式を満たしてください。" }
+  validates :password, presence: { message: 'を入力してください。' }, confirmation: true, length: { 
+    in: 8..72,
+    too_short: "は8文字以上にしてください。",
+    too_long: "は72文字以下にしてください。"
+  },
     format: {
-      # 少なくとも1つ 英字小文字, 大文字, 数字 が含まれる / 合計で8文字以上
-      with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[!-~]{8,}+\z/,
-      message: "は8文字以上で、大文字・小文字・数字を含めて入力してください。"
+      # 少なくとも1つ 英字小文字, 大文字, 数字 が含まれる
+      with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[!-~]+\z/,
+      message: "は、英字小文字・英字大文字・数字がそれぞれ1つ以上含まれるようにしてください。"
     }
 end
