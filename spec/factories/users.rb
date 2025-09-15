e3c749b81f7e9fae84027a3067325dfd6c9e61ee
dueityou <dueityou@gmail.com>
2025-09-15 16:05:08 +0900

    test: #80 ユーザーのテストデータにnameの項目を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "たかし#{n}号" }
     sequence(:email) { |n| "user_#{n}@gmail.com" }
     password {'G554557h'}
     password_confirmation {'G554557h'}
