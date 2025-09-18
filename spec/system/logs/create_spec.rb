087718d4c2baa8d8d763a2ae686f90528a61e8b6
dueityou <dueityou@gmail.com>
2025-09-18 14:59:48 +0900

    test(fix): logのパスは既にないのでdaily_recordsのヘルパーパスに変更

--- spec/system/logs/create_spec.rb
+++ spec/system/logs/create_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe "Create", type: :system do
   describe '記録作成機能' do
     context '記録の作成に成功した場合' do
       it '成功を知らせるフラッシュメッセージが表示されること' do 
-        visit new_log_path
+        visit new_daily_record_path
         find_by_id('mood_range').set rand(-5..5)
         click_on '記録する'
-        expect(page).to have_current_path(new_log_path)
+        expect(page).to have_current_path(new_daily_record_path)
         expect(page).to have_content '記録の作成が完了しました。'
       end
     end
 
     context '記録の作成に失敗した場合' do
       it '失敗を知らせるフラッシュメッセージが表示されること' do
-        visit new_log_path
+        visit new_daily_record_path
         find_by_id('mood_range').set rand(-5..5)
         click_on '記録する'
-        expect(page).to have_current_path(new_log_path)
+        expect(page).to have_current_path(new_daily_record_path)
         expect(page).to have_content '記録の作成に失敗しました。'
       end
     end
