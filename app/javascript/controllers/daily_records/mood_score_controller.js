7a40d800963020155788e9b55c3a3eefb6fb5a5c
dueityou <dueityou@gmail.com>
2025-09-18 20:29:49 +0900

    feat: #14 レンジスライダーの初期値を画面に表示

---
+++ app/javascript/controllers/daily_records/mood_score_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="daily-records--mood-score"
+export default class extends Controller {
+  connect() {
+    const currentMoodScore = document.getElementById("mood-range").value;
+    const target = document.getElementById("current-mood-score");
+    target.textContent = currentMoodScore;
+  }
+}
