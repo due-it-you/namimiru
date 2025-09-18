de8aee8ddde51af86346c97c664f2537cfdf417e
dueityou <dueityou@gmail.com>
2025-09-18 13:31:24 +0900

    fix: #14 it句がないことでエラーが起きていたので修正

--- spec/system/logs/create_spec.rb
+++ spec/system/logs/create_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe "Create", type: :system do
   describe '記録作成機能' do
     context '記録の作成に成功した場合' do
-      visit new_log_path
-      click_on '記録する'
-      expect(page).to have_current_path(new_log_path)
-      expect(page).to have_content '記録の作成が完了しました。'
+      it '成功を知らせるフラッシュメッセージが表示されること' do 
+        visit new_log_path
+        click_on '記録する'
+        expect(page).to have_current_path(new_log_path)
+        expect(page).to have_content '記録の作成が完了しました。'
+      end
     end
   end
 end
