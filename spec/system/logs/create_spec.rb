b1a985a8f38940b52aaccd3d6969a65ea532a4bc
dueityou <dueityou@gmail.com>
2025-09-18 13:40:59 +0900

    test: #14 記録の作成に失敗した場合のテストを実装

--- spec/system/logs/create_spec.rb
+++ spec/system/logs/create_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe "Create", type: :system do
         expect(page).to have_content '記録の作成が完了しました。'
       end
     end
+
+    context '記録の作成に失敗した場合' do
+      it '失敗を知らせるフラッシュメッセージが表示されること' do
+        visit new_log_path
+        click_on '記録する'
+        expect(page).to have_current_path(new_log_path)
+        expect(page).to have_content '記録の作成に失敗しました。'
+      end
+    end
   end
 end
