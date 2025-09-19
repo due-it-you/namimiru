13d5f41044b76c0c9f30af5b624449995a3e3289
dueityou <dueityou@gmail.com>
2025-09-19 13:57:30 +0900

    feat: #15 記録の保存処理の実装

--- app/controllers/daily_records_controller.rb
+++ app/controllers/daily_records_controller.rb
@@ -1,7 +1,18 @@
 class DailyRecordsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def new; end
+
+  def create 
+    daily_record = current_user.daily_records.new(daily_record_params)
+    if daily_record.save
+      flash[:success] = "記録の作成が完了しました。"
+      redirect_to charts_path
+    else
+      flash[:danger] = "記録の作成に失敗しました。"
+      render :new
+    end
+  end
 
   private
 
