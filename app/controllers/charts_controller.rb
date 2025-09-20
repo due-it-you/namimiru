8699be5936cf2368391cd28f9731dcd762a2b130
dueityou <dueityou@gmail.com>
2025-09-20 11:34:54 +0900

    refactor: #16 ERBファイルの中のscriptタグで記述せず、Stimulusコントローラーでグラフの表示に関する処理を記述

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,8 +2,11 @@ class ChartsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @daily_records = current_user.daily_records
+    daily_records = current_user.daily_records
       .where(created_at: 1.months.ago.beginning_of_day..Time.current.end_of_day)
       .order(created_at: :ASC)
+    # グラフ表示のためのラベルとデータ
+    @labels = daily_records.map { |record| record.created_at.strftime("%m/%d").to_json }
+    @data = daily_records.map { |record| record.mood_score }
   end
 end
