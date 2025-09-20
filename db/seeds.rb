4c85a626258f2d1621981002e95126af45a3ac3a
dueityou <dueityou@gmail.com>
2025-09-20 10:11:01 +0900

    feat: #15 １年間分の記録のseedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create!(
+  name: 'eminem',
+  email: 'eminem@test.com',
+  password: 'EminemEminem2'
+)
+
+365.times do |n|
+  user.daily_records.create!(
+    mood_score: rand(-5..5),
+    memo: Faker::Books::Lovecraft.paragraph,
+    created_at: n.days.ago,
+    updated_at: n.days.ago 
+  )
+end
