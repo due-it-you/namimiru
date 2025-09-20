a8c60a66649aa86b02e171ff9d52370de9db8f4d
dueityou <dueityou@gmail.com>
2025-09-20 11:07:06 +0900

    feat: #16 直近１ヶ月のグラフを表示

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,9 @@
 class ChartsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @daily_records = current_user.daily_records
+      .where(created_at: 1.months.ago.beginning_of_day..Time.current.end_of_day)
+      .order(created_at: :ASC)
+  end
 end
