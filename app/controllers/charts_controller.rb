646bc0f0fbaee358df133144ffa5fd917510046d
dueityou <dueityou@gmail.com>
2025-09-20 17:06:02 +0900

    feat: #93 選択されたユーザーのグラフ情報を表示する画面の実装

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,10 +3,14 @@ class ChartsController < ApplicationController
 
   def index
     @current_user = current_user
-    @latest_record = current_user.daily_records.order(created_at: :ASC).last
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    @latest_record = user.daily_records.order(created_at: :ASC).last
 
     # グラフ表示のためのラベルとデータ
-    daily_records = current_user.daily_records
+    daily_records = user.daily_records
       .where(created_at: 1.months.ago.beginning_of_day..Time.current.end_of_day)
       .order(created_at: :ASC)
     @labels = daily_records.map { |record| record.created_at.strftime("%m/%d").to_json }
