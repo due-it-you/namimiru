6eb47bc97470cf49e10a7117b65e166c49781342
dueityou <dueityou@gmail.com>
2025-09-20 11:22:49 +0900

    rename: #16 Stimulusのchartコントローラーをcharts/display_controllerに置換

--- app/javascript/controllers/charts/display_controller.js
+++ app/javascript/controllers/charts/display_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="chart"
+// Connects to data-controller="charts--display"
 export default class extends Controller {
   connect() {
   }
