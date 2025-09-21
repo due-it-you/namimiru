ed9a6650e90bc513740a4f2c922cb43ab778fb9e
dueityou <dueityou@gmail.com>
2025-09-21 20:33:29 +0900

    feat: #20 誰の記録一覧なのかを表示

--- app/controllers/daily_records_controller.rb
+++ app/controllers/daily_records_controller.rb
@@ -2,8 +2,8 @@ class DailyRecordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    user = User.find(params[:user_id])
-    @daily_records = user.daily_records.order(created_at: :DESC)
+    @user = User.find(params[:user_id])
+    @daily_records = @user.daily_records.order(created_at: :DESC)
   end
 
   def new; end
