6b49f49bb06776dc752a8a276a13af51f8f1cc18
dueityou <dueityou@gmail.com>
2025-09-25 16:39:51 +0900

    feat: #29 ユーザーインスタンスをメーラーのビューファイルに渡す

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,6 +2,7 @@ class InvitationMailer < ApplicationMailer
   default from: "namimiruOfficial@gmail.com"
 
   def invite
+    @inviter = params[:user]
     invitee_email = params[:email]
     mail(to: invitee_email, subject: "招待コードを入力して連携しましょう！ ナミミルからのお知らせ")
   end
