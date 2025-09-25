1dd6e85bc55efdea4e892131f79fb1dc6b826351
dueityou <dueityou@gmail.com>
2025-09-25 21:40:10 +0900

    fix: #29 変数名が明示的ではなかったので修正

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,9 +1,7 @@
 class InvitationMailer < ApplicationMailer
-  default from: "namimiruOfficial@gmail.com"
-
   def invite
-    @inviter = params[:user]
-    invitee_email = params[:email]
-    mail(to: invitee_email, subject: "招待コードを入力して連携しましょう！ ナミミルからのお知らせ")
+    @inviter = params[:inviter]
+    invitee_email = params[:invitee_email]
+    mail(from: 'namimiruofficial@gmail.com', to: invitee_email, subject: "招待コードを入力して連携しましょう！ ナミミルからのお知らせ")
   end
 end
