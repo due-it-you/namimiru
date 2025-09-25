d6c31d4e0d8e94f953e1b33450bfc6617570734e
dueityou <dueityou@gmail.com>
2025-09-25 16:10:59 +0900

    feat: #29 招待メールを送信するアクションの追加

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,2 +1,8 @@
 class InvitationMailer < ApplicationMailer
+  default from: "namimiruOfficial@gmail.com"
+
+  def invite
+    invitee_email = params[:email]
+    mail(to: invitee_email, subject: "招待コードを入力して連携しましょう！ ナミミルからのお知らせ")
+  end
 end
