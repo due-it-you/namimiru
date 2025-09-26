4562a568a5918e0522c46bc41ad24a5e619ee6a5
dueityou <dueityou@gmail.com>
2025-09-26 15:02:50 +0900

    fix: #136 typoの修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  include Devise::Controller::Rememberable
+  include Devise::Controllers::Rememberable
 
   skip_before_action :authenticate_user!
   # You should configure your model like this:
