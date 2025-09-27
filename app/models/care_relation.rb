6225c92191cec79b4639d23564ced6e9375e63cc
dueityou <dueityou@gmail.com>
2025-09-27 23:26:32 +0900

    feat: #31 care_relationsの外部キーの組み合わせに一意制約を付与

--- app/models/care_relation.rb
+++ app/models/care_relation.rb
@@ -1,4 +1,6 @@
 class CareRelation < ApplicationRecord
   belongs_to :supported, class_name: 'User'
   belongs_to :supporter, class_name: 'User'
+
+  validates :supported_id, uniqueness: { scope: :supporter_id }
 end
