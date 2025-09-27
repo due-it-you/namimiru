3741549d47c99da72b924266ca0905c9862f315f
dueityou <dueityou@gmail.com>
2025-09-27 16:13:13 +0900

    feat: #31 care_relationsテーブルの新規作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_25_095351) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_27_060247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "care_relations", force: :cascade do |t|
+    t.bigint "supported_id"
+    t.bigint "supporter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["supported_id"], name: "index_care_relations_on_supported_id"
+    t.index ["supporter_id"], name: "index_care_relations_on_supporter_id"
+  end
 
   create_table "daily_records", force: :cascade do |t|
     t.integer "mood_score", null: false
@@ -45,10 +54,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_25_095351) do
     t.boolean "is_guest", default: false, null: false
     t.string "invitation_token"
     t.datetime "invitation_created_at"
+    t.string "remember_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "care_relations", "users", column: "supported_id"
+  add_foreign_key "care_relations", "users", column: "supporter_id"
   add_foreign_key "daily_records", "users"
   add_foreign_key "social_profiles", "users"
 end
