9fd62e92b5da5186c8253c34d7c9d0b506bf09dd
dueityou <dueityou@gmail.com>
2025-09-30 16:13:58 +0900

    refactor: lint error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   # Gmail用のAction Mailerの設定
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { protocol: 'https', host:'namimiru-app.onrender.com'}
+  config.action_mailer.default_url_options = { protocol: "https", host: "namimiru-app.onrender.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:         "smtp.gmail.com",
