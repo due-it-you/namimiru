a2351b5621e0ab7fcc21133b08a55befd68a5b26
dueityou <dueityou@gmail.com>
2025-09-30 17:17:26 +0900

    fix: 同上

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,16 +82,18 @@ Rails.application.configure do
 
   # Gmail用のAction Mailerの設定
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { protocol: "https", host: "namimiru-app.onrender.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    enable_starttls_auto: true,
     address:         "smtp.gmail.com",
     port:            587,
     domain:          "gmail.com",
     user_name:       "namimiruofficial@gmail.com",
     password:        ENV["GOOGLE_PASSWORD"],
-    authentication:  "plain"
+    authentication:  :login
+    enable_starttls_auto: true,
     }
 
   # Ignore bad email addresses and do not raise email delivery errors.
