dcc0c7cced2fc9ebdd937c22fc7de681e6024bfd
dueityou <dueityou@gmail.com>
2025-09-30 16:13:29 +0900

    hotfix: DockerのWebコンテナが起動しない不具合を修正

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_27_090947) do
     t.datetime "invitation_created_at"
     t.string "remember_token"
     t.string "invitee_role"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
