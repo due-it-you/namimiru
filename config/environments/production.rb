82b875f379c3ecef1c37c45fa236f60e2b94a0c1
dueityou <dueityou@gmail.com>
2025-09-30 17:21:33 +0900

    fix: syntax errpr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
     domain:          "gmail.com",
     user_name:       "namimiruofficial@gmail.com",
     password:        ENV["GOOGLE_PASSWORD"],
-    authentication:  :login
+    authentication:  :login,
     enable_starttls_auto: true,
     }
 
