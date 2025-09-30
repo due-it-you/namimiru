61d57bcb40e8713ed59cb560458cbf9305351a51
dueityou <dueityou@gmail.com>
2025-09-30 22:12:37 +0900

    feat: #12 ゲストログインの実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,15 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :authenticate_user!
   # before_action :configure_sign_in_params, only: [:create]
+  
+  def guest_sign_in
+    user = User.find_or_create_by(email: "guest@example.com") do |user|
+      user.name = "ゲストユーザー"
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to root_path, success: "ゲストユーザーとしてログインしました"
+  end
 
   # GET /resource/sign_in
   # def new
