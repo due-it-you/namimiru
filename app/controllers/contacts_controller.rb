2242f026f3e6458a6738e41f4df9469c9369a9f2
dueityou <dueityou@gmail.com>
2025-10-06 20:10:31 +0900

    feat: #53 問い合わせに関するストロングパラメータの追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,10 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :subject, :message)
+  end
 end
