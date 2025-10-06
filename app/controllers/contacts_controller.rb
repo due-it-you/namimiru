2567274eccc0cb0c64af3ff5578c1324dc33ffba
dueityou <dueityou@gmail.com>
2025-10-06 20:41:02 +0900

    feat: #53 メール送信処理の実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,18 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create 
+    contact = Contact.new(contact_params)
+    if contact.save
+      ContactMailer.send_mail(contact).deliver_later
+      flash[:success] = "お問い合わせ内容を送信しました。"
+      redirect_to new_contact_path
+    else
+      flash[:alert] = "お問い合わせ内容を送信できませんでした。"
+      redirect_to new_contact_path
+    end
+  end
+
   private
 
   def contact_params
