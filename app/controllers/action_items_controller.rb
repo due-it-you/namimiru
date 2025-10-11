8dc80db99f1d0528a47bfafc19dfba360f65b1f0
dueityou <dueityou@gmail.com>
2025-10-11 21:39:24 +0900

    feat: #222 action_itemのストロングパラメータの追加

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -4,4 +4,10 @@ class ActionItemsController < ApplicationController
   def new
     @action_item = ActionItem.new
   end
+
+  private
+
+  def action_item_params
+    params.require(:action_item).permit(:name, :enabled_from)
+  end
 end
