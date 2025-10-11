0b8c060710d3e0f918fc45a19f7b33f4b808eb7b
dueityou <dueityou@gmail.com>
2025-10-11 22:02:46 +0900

    feat: #222 行動項目の保存処理の実装

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -5,6 +5,17 @@ class ActionItemsController < ApplicationController
     @action_item = ActionItem.new
   end
 
+  def create 
+    action_item = current_user.action_items.new(action_item_params)
+    if action_item.save
+      flash[:success] = "項目を作成しました。"
+      redirect_to action_items_path
+    else
+      flash[:alert] = "項目の作成が出来ませんでした。"
+      redirect_to action_items_path
+    end
+  end
+
   private
 
   def action_item_params
