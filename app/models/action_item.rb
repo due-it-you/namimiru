06fe55ff20682d1fbf3d8513f74480f617dd9a7c
dueityou <dueityou@gmail.com>
2025-10-13 00:17:33 +0900

    refactor: lint error

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -5,7 +5,7 @@ class ActionItem < ApplicationRecord
   validates :enabled_from, presence: true, numericality: { in: -5..5 }
 
   # できるリストに表示される行動項目の検索
-  scope :capable, -> (mood_score) { where("enabled_from <= ?", mood_score.to_i) }
+  scope :capable, ->(mood_score) { where("enabled_from <= ?", mood_score.to_i) }
   # できないリストに表示される行動項目の検索
-  scope :incapable, -> (mood_score) { where("enabled_from > ?", mood_score.to_i) }
+  scope :incapable, ->(mood_score) { where("enabled_from > ?", mood_score.to_i) }
 end
