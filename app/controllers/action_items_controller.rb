eba8950713499383864b7170063ec6fde9eb2cd5
dueityou <dueityou@gmail.com>
2025-10-13 00:16:22 +0900

    comment: #216 コメントの追加

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -1,6 +1,7 @@
 class ActionItemsController < ApplicationController
   def index
     latest_mood_score = current_user.daily_records.order(created_at: :desc).first&.mood_score
+    # スライダーを変動させた後の値 || 初回アクセス時の初期値 || 記録がまだ存在しない場合の初期値
     @mood_score =  params[:mood_score] || latest_mood_score || 0
     @can_list = current_user.action_items.capable(@mood_score)
     @cannot_list = current_user.action_items.incapable(@mood_score)
