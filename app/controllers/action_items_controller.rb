c84f7255ec55f5ccffea6f398ab38975891cda5e
dueityou <dueityou@gmail.com>
2025-10-14 14:27:35 +0900

    feat: #235 できるかもリストの項目で増えた差分を緑枠のCSSが付与されるように更新

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -5,6 +5,9 @@ class ActionItemsController < ApplicationController
     @mood_score =  params[:mood_score] || latest_mood_score || 0
     @can_list = current_user.action_items.capable(@mood_score)
     @cannot_list = current_user.action_items.incapable(@mood_score)
+    @diff_can_items = @can_list.to_a - current_user.action_items.capable(latest_mood_score)
+    @not_diff_can_items = @can_list.to_a - @diff_can_items
+
     if turbo_frame_request?
       render partial: "action_items/lists_frame"
     end
