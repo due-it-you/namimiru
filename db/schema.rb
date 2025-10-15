45129179c65dae09188f24e90ad0a65c81e723e7
dueityou <dueityou@gmail.com>
2025-10-16 08:40:11 +0900

    feat: #219 action_itemsテーブルにaction_tag_idの外部キーのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_15_231249) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_15_233541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_15_231249) do
     t.integer "enabled_from", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "action_tag_id"
+    t.index ["action_tag_id"], name: "index_action_items_on_action_tag_id"
     t.index ["user_id"], name: "index_action_items_on_user_id"
   end
 
@@ -86,6 +88,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_15_231249) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "action_items", "action_tags"
   add_foreign_key "action_items", "users"
   add_foreign_key "care_relations", "users", column: "supported_id"
   add_foreign_key "care_relations", "users", column: "supporter_id"
