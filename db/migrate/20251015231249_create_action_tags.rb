8ccf45b43700b0b3882433eba719f66cdd86adf4
dueityou <dueityou@gmail.com>
2025-10-16 08:35:05 +0900

    feat: #219 action_tagsテーブルの新規作成

--- db/migrate/20251015231249_create_action_tags.rb
+++ db/migrate/20251015231249_create_action_tags.rb
@@ -1,6 +1,7 @@
 class CreateActionTags < ActiveRecord::Migration[7.2]
   def change
     create_table :action_tags do |t|
+      t.string :name, default: "未分類", null: false, index: { unique: true }
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_11_103712) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_15_231249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_11_103712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_action_items_on_user_id"
+  end
+
+  create_table "action_tags", force: :cascade do |t|
+    t.string "name", default: "未分類", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_action_tags_on_name", unique: true
   end
 
   create_table "care_relations", force: :cascade do |t|
