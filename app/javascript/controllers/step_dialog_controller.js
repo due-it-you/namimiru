2231c7dd0e0c0040709d16d2939f7365071362ca
dueityou <dueityou@gmail.com>
2025-10-25 17:16:18 +0900

    feat: #188 使い方のステップの切り替え処理の実装

--- app/javascript/controllers/step_dialog_controller.js
+++ app/javascript/controllers/step_dialog_controller.js
@@ -2,6 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="step-dialog"
 export default class extends Controller {
+  static targets = ["step"]
+
   connect() {
+    this.currentStep = 1;
+  }
+
+  showCurrentStep() {
+    this.stepTargets.forEach((stepElement, index) => {
+      if (index === this.currentStep - 1) {
+        stepElement.style.display = "block";
+      } else {
+        stepElement.style.display = "none";
+      }
+    });
+  }
+
+  nextStep() {
+    if (this.currentStep < this.stepTargets.length) {
+      this.currentStep++;
+      this.showCurrentStep();
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+      this.showCurrentStep();
+    }
   }
 }
