df6dcc4adb4d5a772e1138f8686a4430faee531e
dueityou <dueityou@gmail.com>
2025-10-29 10:47:45 +0900

    feat: #309 ログインセッションが維持される時間を1ヶ月に変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.timeout_in = 48.hours
+  config.timeout_in = 1.month
   config.omniauth :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET_KEY"]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
