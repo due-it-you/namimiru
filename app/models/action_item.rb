2b92b71f4900a6e8bce9871a7aca45654df3ab27
dueityou <dueityou@gmail.com>
2025-10-29 16:35:17 +0900

    feat: #307 behavior_typeのenumの定義を追加

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -5,6 +5,11 @@ class ActionItem < ApplicationRecord
   validates :name, presence: true
   validates :enabled_from, presence: true, numericality: { in: -5..5 }
 
+  enum :behavior_type, { 
+    dynamic: 0, # できるかも/できないかもリスト内の項目
+    avoid: 1 # やらない方がいいリスト内の項目
+  }
+
   # できるリストに表示される行動項目の検索
   scope :capable, ->(mood_score) { where("enabled_from <= ?", mood_score.to_i) }
   # できないリストに表示される行動項目の検索
