566c256a23d52fb3f8b0ec52ff989a785380911d
dueityou <dueityou@gmail.com>
2025-10-29 16:21:50 +0900

    feat: #307 enabled_fromカラムをNULL許可するように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_29_065223) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_29_071917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "action_items", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name", null: false
-    t.integer "enabled_from", null: false
+    t.integer "enabled_from"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "action_tag_id"
