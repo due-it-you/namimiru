275d81161c94fbcb14bd695b6c927dc75a2b12fa
dueityou <dueityou@gmail.com>
2025-10-29 16:05:44 +0900

    feat: #307 behavior_typeカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_24_055548) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_29_065223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_24_055548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "action_tag_id"
+    t.integer "behavior_type", default: 0, null: false
     t.index ["action_tag_id"], name: "index_action_items_on_action_tag_id"
     t.index ["user_id"], name: "index_action_items_on_user_id"
   end
