0add9664d281d192db9d2672dac7a2ca6987ec67
dueityou <dueityou@gmail.com>
2025-11-07 12:03:39 +0900

    feat: #293 直近の記録のXへのシェア機能

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,2 +1,10 @@
 module ChartsHelper
+  def daily_record_x_share_url(latest_record)
+    text = <<~TEXT
+      【#{mood_display(latest_record.mood_score)}】
+
+      今日の記録が出来ました！
+    TEXT
+    "https://twitter.com/share?text=#{CGI.escape(text)}&url=https://namimiru.com"
+  end
 end
