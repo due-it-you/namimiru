eb26ef0bfd45c7bdd92f9eb6ab01a34dc70896b5
dueityou <dueityou@gmail.com>
2025-11-08 15:47:12 +0900

    test: #369 項目名に入力される文字列がランダムであるように更新

--- spec/system/action_items/create_enable_item_spec.rb
+++ spec/system/action_items/create_enable_item_spec.rb
@@ -12,8 +12,10 @@ RSpec.describe "CreateEnableItem", type: :system do
 
     # 正常系
     context "作成が成功した場合" do
+      let(:valid_action_item_name) { SecureRandom.alphanumeric(15) }
+
       before do
-        fill_in "action_item[name]", with: "自炊"
+        fill_in "action_item[name]", with: valid_action_item_name
       end
 
       it "/action_items.htmlにリダイレクトされていること" do
@@ -23,7 +25,7 @@ RSpec.describe "CreateEnableItem", type: :system do
 
       it "作成した項目名が画面に表示されていること" do
         click_on "commit"
-        expect(page).to have_content "自炊"
+        expect(page).to have_content valid_action_item_name
       end
 
       it "成功のフラッシュカードが表示されていること" do
@@ -35,14 +37,14 @@ RSpec.describe "CreateEnableItem", type: :system do
     # 異常系
     context "作成が失敗した場合" do
       context "項目名が上限の15文字を超えている場合" do
-        let(:random_action_item_name) { SecureRandom.alphanumeric(16) }
+        let(:invalid_action_item_name) { SecureRandom.alphanumeric(16) }
         before do
-          fill_in "action_item[name]", with: random_action_item_name
+          fill_in "action_item[name]", with: invalid_action_item_name
         end
 
         it "画面に作成しようとした項目名が表示されていないこと" do
           click_on "commit"
-          expect(page).not_to have_content random_action_item_name
+          expect(page).not_to have_content invalid_action_item_name
         end
 
         it "失敗のフラッシュメッセージが表示されていること" do
