b6009de32e05e8b3be134e49d2226bc81c67cdfb
dueityou <dueityou@gmail.com>
2025-11-09 00:41:22 +0900

    test: #373 タグ作成に失敗した時のテストを実装

--- spec/system/action_tags/create_spec.rb
+++ spec/system/action_tags/create_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "CreateActionTags", type: :system do
       fill_in "action_item[name]", with: "自炊"
     end
 
+    # 正常系
     context "タグの新規作成が成功した場合" do
       before do
         # comboboxを開く
@@ -34,6 +35,28 @@ RSpec.describe "CreateActionTags", type: :system do
         # comboboxを開く
         find('input[role="combobox"]#action_item_action_tag_id').click
         expect(page).to have_content '食事'
+      end
+    end
+
+    # 異常系
+    context "タグの新規作成が失敗した場合" do
+      context "タグ名が上限の8文字を超えている場合" do
+        before do
+          # comboboxを開く
+          find('input[role="combobox"]#action_item_action_tag_id').click
+          # 9文字のタグ名を入力 
+          find('input[role="combobox"]:visible').fill_in(with: '123456789')
+        end
+
+        it "/action_items/newの画面が表示されていること" do
+          click_on "commit"
+          expect(page).to have_current_path(new_action_item_path)
+        end
+
+        it "エラーメッセージが表示されていること" do
+          click_on "commit"
+          expect(page).to have_content "タグ名は8文字以内で入力してください。"
+        end
       end
     end
   end
