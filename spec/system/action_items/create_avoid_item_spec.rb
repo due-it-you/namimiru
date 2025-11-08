3b8075d47dfdc8222fbe60168bfaadfb250622bd
dueityou <dueityou@gmail.com>
2025-11-08 15:33:35 +0900

    test: #369 やらない方がいいリストの項目の作成失敗時のテストを実装

--- spec/system/action_items/create_avoid_item_spec.rb
+++ spec/system/action_items/create_avoid_item_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 RSpec.describe "CreateAvoidItem", type: :system do
   describe "躁の時にやらない方がいい行動リスト内の項目の作成機能" do
     let(:user) { create(:user) }
-    let(:random_action_item_name) { SecureRandom.alphanumeric(15) }
 
     before do
       sign_in user
       visit new_action_item_path
       select "躁状態の時にやらない方がいい行動", from: "action_item[behavior_type]"
-      fill_in "action_item[name]", with: random_action_item_name
     end
 
     # 正常系
     context "作成に成功した場合" do
+      let(:valid_action_item_name) { SecureRandom.alphanumeric(15) }
+
+      before do
+        fill_in "action_item[name]", with: valid_action_item_name
+      end
+        
       it "/action_items.htmlにリダイレクトされていること" do
         click_on "commit"
         expect(page).to have_current_path(action_items_path(format: :html))
@@ -27,7 +31,28 @@ RSpec.describe "CreateAvoidItem", type: :system do
       it "作成した項目名が表示されていること" do
         click_on "commit"
         find('#mood-range').set(1)
-        expect(page).to have_content random_action_item_name
+        expect(page).to have_content valid_action_item_name
+      end
+    end
+
+    # 異常系
+    context "作成に失敗した場合" do
+      context "項目名が上限の15文字を超えていた場合" do
+        let(:invalid_action_item_name) { SecureRandom.alphanumeric(16) }
+
+        before do
+          fill_in "action_item[name]", with: invalid_action_item_name
+        end
+        
+        it "/action_items/newの画面が表示されていること" do
+          click_on "commit"
+          expect(page).to have_current_path(new_action_item_path)
+        end
+
+        it "項目名に関するエラーメッセージが表示されていること" do
+          click_on "commit"
+          expect(page).to have_content "項目名は15文字以内で入力してください。"
+        end
       end
     end
   end
