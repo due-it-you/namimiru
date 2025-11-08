ebafb89fe0df4b0c2b1dbd89121677236a5a7ccf
dueityou <dueityou@gmail.com>
2025-11-08 12:41:50 +0900

    feat: #369 行動マップの項目名は最大15文字となるバリデーションを追加

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -2,7 +2,7 @@ class ActionItem < ApplicationRecord
   belongs_to :user
   belongs_to :action_tag
 
-  validates :name, presence: true
+  validates :name, length: { maximum: 15 }, presence: true
 
   enum :behavior_type, {
     dynamic: 0, # できるかも/できないかもリスト内の項目
