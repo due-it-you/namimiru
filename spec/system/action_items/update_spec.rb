f9c4cbcd60ac73746259eb70dd71f967f3d80248
dueityou <dueityou@gmail.com>
2025-11-09 17:08:49 +0900

    test: #374 やらない方がいい行動リストに項目の種類を更新した時のテストを実装

--- spec/system/action_items/update_spec.rb
+++ spec/system/action_items/update_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "UpdateActionItemAndTag", type: :system do
   describe "項目・タグの更新機能" do
     let(:user) { create(:user) }
+    let!(:action_tag) { create(:action_tag, user: user) }
+    let!(:action_item) { create(:action_item, user: user, action_tag: action_tag) }
+
 
     before do
       sign_in user
@@ -42,9 +45,6 @@ RSpec.describe "UpdateActionItemAndTag", type: :system do
       end
 
       context "更新が失敗した場合" do
-        let!(:action_tag) { create(:action_tag, user: user) }
-        let!(:action_item) { create(:action_item, user: user, action_tag: action_tag) }
-
         context "項目名が15文字を超えている場合" do
           let(:invalid_random_action_item_name) { SecureRandom.alphanumeric(16) }
 
@@ -84,10 +84,47 @@ RSpec.describe "UpdateActionItemAndTag", type: :system do
             click_on "commit"
             expect(page).to have_current_path(edit_action_item_path(action_item.id))
           end
-          
+
           it "エラーメッセージが表示されていること" do
             click_on "commit" 
             expect(page).to have_content "タグ名は8文字以内で入力してください。"
+          end
+        end
+      end
+    end
+
+    describe "項目の種類の更新" do
+      context "「できそうかも/できなさそうかも」 -> 「躁状態の時にやらない方がいい行動」に更新した場合" do
+
+        before do
+          visit action_items_path(format: :html)
+          # 項目UIをクリックして編集画面に遷移
+          click_on action_item.name
+          select "躁状態の時にやらない方がいい行動", from: "action_item[behavior_type]"
+        end
+
+        it "/action_items.htmlにリダイレクトされること" do
+          click_on "commit"
+          expect(page).to have_current_path(action_items_path(format: :html))
+        end
+
+        it "「やらない方がいい」リスト内で更新した項目が表示されていること" do
+          click_on "commit"
+          find("#switch-unenable-and-avoid-lists-icon").click
+          within("#avoid-list") do
+            expect(page).to have_content action_item.name
+          end
+        end
+
+        it "「できるかも/できないかも」リスト内に項目が表示されていないこと" do
+          click_on "commit"
+          find("#switch-unenable-and-avoid-lists-icon").click
+          within("#can-list") do
+            expect(page).not_to have_content action_item.name
+          end
+          find("#switch-unenable-and-avoid-lists-icon").click
+          within("#cannot-list") do
+            expect(page).not_to have_content action_item.name
           end
         end
       end
