94b9712910bc328f775b90a8130538fec1b71881
dueityou <dueityou@gmail.com>
2025-11-11 20:47:41 +0900

    db: #385 action_itemsテーブルのindexを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_11_114150) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_11_114608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_11_114150) do
     t.integer "behavior_type", default: 0, null: false
     t.uuid "user_uuid", null: false
     t.uuid "action_tag_uuid", null: false
-    t.index ["action_tag_id"], name: "index_action_items_on_action_tag_id"
+    t.index ["action_tag_uuid"], name: "index_action_items_on_action_tag_uuid"
     t.index ["user_uuid"], name: "index_action_items_on_user_uuid"
   end
 
