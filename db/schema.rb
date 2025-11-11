c6fd413093091a62061f7739d07fd086013f80cc
dueityou <dueityou@gmail.com>
2025-11-11 21:23:15 +0900

    db: #385 care_relationsテーブルの主キーをuuidに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_11_121824) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_11_122210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -39,12 +39,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_11_121824) do
     t.index ["uuid"], name: "index_action_tags_on_uuid", unique: true
   end
 
-  create_table "care_relations", force: :cascade do |t|
+  create_table "care_relations", primary_key: "uuid", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigserial "id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "supported_uuid", null: false
     t.uuid "supporter_uuid", null: false
-    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.index ["supported_uuid", "supporter_uuid"], name: "index_care_relations_on_supported_uuid_and_supporter_uuid", unique: true
     t.index ["supported_uuid"], name: "index_care_relations_on_supported_uuid"
     t.index ["supporter_uuid"], name: "index_care_relations_on_supporter_uuid"
