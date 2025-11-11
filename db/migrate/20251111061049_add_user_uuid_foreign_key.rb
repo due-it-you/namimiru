51c38e53db8f0a785091b79219ee94d829818016
dueityou <dueityou@gmail.com>
2025-11-12 08:23:58 +0900

    wip

--- db/migrate/20251111061049_add_user_uuid_foreign_key.rb
+++ db/migrate/20251111061049_add_user_uuid_foreign_key.rb
@@ -1,5 +1,5 @@
 class AddUserUuidForeignKey < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :daily_records,   :user_uuid,      :uuid
     add_column :action_items,    :user_uuid,      :uuid
     add_column :action_tags,     :user_uuid,      :uuid
@@ -7,81 +7,59 @@ class AddUserUuidForeignKey < ActiveRecord::Migration[7.2]
     add_column :care_relations,  :supported_uuid, :uuid
     add_column :care_relations,  :supporter_uuid, :uuid
 
-    Rails.logger.info("STEP1 start daily_records")
-    begin
-      DailyRecord.reset_column_information
-      DailyRecord.find_each do |r|
-        uuid = User.unscoped.where(id: r.user_id).pick(:uuid)
-        next unless uuid
-        r.update_columns(user_uuid: uuid)
-      end
-      Rails.logger.info("STEP1 end daily_records")
-    rescue => e
-      Rails.logger.error("STEP1 FAIL #{e.class}: #{e.message}")
-      raise
-    end
+    say "BF daily_records"
+    execute <<~SQL
+    UPDATE daily_records dr
+    SET user_uuid = u.uuid
+    FROM users u
+    WHERE dr.user_uuid IS NULL
+    AND dr.user_id = u.id;
+    SQL
 
-    Rails.logger.info("STEP2 start action_items")
-    begin
-      ActionItem.reset_column_information
-      ActionItem.find_each do |r|
-        uuid = User.unscoped.where(id: r.user_id).pick(:uuid)
-        next unless uuid
-        r.update_columns(user_uuid: uuid)
-      end
-      Rails.logger.info("STEP2 end action_items")
-    rescue => e
-      Rails.logger.error("STEP2 FAIL #{e.class}: #{e.message}")
-      raise
-    end
+    say "BF action_items"
+    execute <<~SQL
+    UPDATE action_items ai
+    SET user_uuid = u.uuid
+    FROM users u
+    WHERE ai.user_uuid IS NULL
+    AND ai.user_id = u.id;
+    SQL
 
-    Rails.logger.info("STEP3 start action_tags")
-    begin
-      ActionTag.reset_column_information
-      ActionTag.find_each do |r|
-        uuid = User.unscoped.where(id: r.user_id).pick(:uuid)
-        next unless uuid
-        r.update_columns(user_uuid: uuid)
-      end
-      Rails.logger.info("STEP3 end action_tags")
-    rescue => e
-      Rails.logger.error("STEP3 FAIL #{e.class}: #{e.message}")
-      raise
-    end
+    say "BF action_tags"
+    execute <<~SQL
+    UPDATE action_tags at
+    SET user_uuid = u.uuid
+    FROM users u
+    WHERE at.user_uuid IS NULL
+    AND at.user_id = u.id;
+    SQL
 
-    Rails.logger.info("STEP4 start social_profiles")
-    begin
-      SocialProfile.reset_column_information
-      SocialProfile.find_each do |r|
-        next unless r.user_id
-        uuid = User.unscoped.where(id: r.user_id).pick(:uuid)
-        next unless uuid
-        r.update_columns(user_uuid: uuid)
-      end
-      Rails.logger.info("STEP4 end social_profiles")
-    rescue => e
-      Rails.logger.error("STEP4 FAIL #{e.class}: #{e.message}")
-      raise
-    end
+    say "BF social_profiles"
+    execute <<~SQL
+    UPDATE social_profiles sp
+    SET user_uuid = u.uuid
+    FROM users u
+    WHERE sp.user_uuid IS NULL
+    AND sp.user_id = u.id;
+    SQL
 
-    Rails.logger.info("STEP5 start care_relations")
-    begin
-      CareRelation.reset_column_information
-      CareRelation.find_each do |r|
-        if r.supported_id
-          sup = User.unscoped.where(id: r.supported_id).pick(:uuid)
-          r.update_columns(supported_uuid: sup) if sup
-        end
-        if r.supporter_id
-          spr = User.unscoped.where(id: r.supporter_id).pick(:uuid)
-          r.update_columns(supporter_uuid: spr) if spr
-        end
-      end
-      Rails.logger.info("STEP5 end care_relations")
-    rescue => e
-      Rails.logger.error("STEP5 FAIL #{e.class}: #{e.message}")
-      raise
-    end
+    say "BF care_relations.supported_uuid"
+    execute <<~SQL
+    UPDATE care_relations cr
+    SET supported_uuid = u.uuid
+    FROM users u
+    WHERE cr.supported_uuid IS NULL
+    AND cr.supported_id = u.id;
+    SQL
+
+    say "BF care_relations.supporter_uuid"
+    execute <<~SQL
+    UPDATE care_relations cr
+    SET supporter_uuid = u.uuid
+    FROM users u
+    WHERE cr.supporter_uuid IS NULL
+    AND cr.supporter_id = u.id;
+    SQL
 
     change_column_null :daily_records,   :user_uuid,      false
     change_column_null :action_items,    :user_uuid,      false
@@ -89,5 +67,14 @@ class AddUserUuidForeignKey < ActiveRecord::Migration[7.2]
     change_column_null :social_profiles, :user_uuid,      false
     change_column_null :care_relations,  :supported_uuid, false
     change_column_null :care_relations,  :supporter_uuid, false
+  end
+
+  def down
+    remove_column :daily_records,   :user_uuid
+    remove_column :action_items,    :user_uuid
+    remove_column :action_tags,     :user_uuid
+    remove_column :social_profiles, :user_uuid
+    remove_column :care_relations,  :supported_uuid
+    remove_column :care_relations,  :supporter_uuid
   end
 end
