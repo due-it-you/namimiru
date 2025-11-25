552104170356f0763815a8e42bb0adec4426ef6f
Yuto_Byte <dueityou@gmail.com>
2025-11-25 12:49:14 +0900

    Docs: コメントの追加
    招待コードに関するカラムの更新

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,6 +6,7 @@ class InvitationsController < ApplicationController
     invitee_role = invitation_params[:invitee_role]
     invitation_token = current_user.generate_unique_invitation_token
 
+    # usersテーブルの招待に関するカラムの値を更新
     current_user.assign_attributes(invitation_token: invitation_token, invitation_created_at: Time.now, invitee_role: invitee_role)
     if current_user.save(context: :invitation)
       InvitationMailer.with(inviter: current_user, invitee_email: invitee_email).invite.deliver_later
